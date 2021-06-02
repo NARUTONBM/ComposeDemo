9acf494276686618ed155b2972fa314691edf4de
Randall <narutonbm@gmail.com>
2021-06-03 03:19:02 +0800

    Current Progress: Week 1st Step 7 #5

--- app/src/main/java/com/demo/composedemo/stateincompose/TodoScreen.kt
+++ app/src/main/java/com/demo/composedemo/stateincompose/TodoScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
+import androidx.compose.material.LocalContentColor
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -17,6 +18,20 @@ import com.demo.composedemo.util.generateRandomTodoItem
 import kotlin.random.Random
 
 /**
+ * 当 Compose 第一次运行组合时，它搭建了所有被调用的 composable 的视图树
+ * 然后在重组时，更新树中被调用的 composable
+ *
+ * 副作用（Side-effect）是指在 composable 作用于之外对数据进行了改动
+ * 重组时应避免 side-effect
+ * 诸如 修改 ViewModel 中的状态、调用 Random.nextInt()、或者修改数据库都是典型的 side-effect
+ *
+ * remember 给 composable 提供记忆能力
+ * 通过 remember 实例化的值将被存储在组合树中，且只有在 remember 的 key 被修改时才会重新计算
+ * 可以类比成对象中的 private val 属性
+ * 组合树中存储的值会在 composable 被移出树时立刻被清除，也会在 composable 重新加到树中时被重新创建
+ * 比如会在 LazyColumn 中移除顶部的 item 时发生上述情况，所以不要用 remember 去存储重要的数据
+ *
+ * 幂等的 composable 对于相同的输入会保持相同的输出，当然更没有 side-effect
  *
  * @author Randall.
  * @Date 2021-06-02.
@@ -45,7 +60,7 @@ fun TodoScreen(
                 TodoRow(
                     todo = it,
                     onItemClicked = { onRemoveItem(it) },
-                    modifier = Modifier.fillParentMaxWidth()
+                    modifier = Modifier.fillParentMaxWidth(),
                 )
             }
         }
@@ -70,7 +85,12 @@ fun TodoScreen(
  * @param modifier modifier for this element
  */
 @Composable
-fun TodoRow(todo: TodoItem, onItemClicked: (TodoItem) -> Unit, modifier: Modifier = Modifier) {
+fun TodoRow(
+    todo: TodoItem,
+    onItemClicked: (TodoItem) -> Unit,
+    modifier: Modifier = Modifier,
+    iconAlpha: Float = remember(todo.id) { randomTint() }
+) {
     Row(
         modifier = modifier
             .clickable { onItemClicked(todo) }
@@ -80,6 +100,9 @@ fun TodoRow(todo: TodoItem, onItemClicked: (TodoItem) -> Unit, modifier: Modifie
         Text(todo.task)
         Icon(
             imageVector = todo.icon.imageVector,
+            // LocalContentColor 提供了当前内容颜色的实例
+            // 会被绘制背景的 Surface 这类 Composable 修改
+            tint = LocalContentColor.current.copy(alpha = iconAlpha),
             contentDescription = stringResource(id = todo.icon.contentDescription)
         )
     }
